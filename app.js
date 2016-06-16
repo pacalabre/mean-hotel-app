b72279439602877a14bcb65d1e5793a2bf48f194
Paul <pacalabre@gmail.com>
2016-06-16 13:30:29 -0700

    added bootstrap template styles

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+var path = require('path');
+
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/json', function(req, res) {
+  res.json({"jsonData" : true});
+})
+
+app.get('/file', function(req, res) {
+  res.sendFile(path.join(__dirname, 'app.js'));
+})
+
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log("beAnz is just chillin on "+ port);
+});
+
