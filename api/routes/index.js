b11f2f131bfa8a4d7dd5092c60c202e54711b4bd
Paul <pacalabre@gmail.com>
2016-06-21 17:03:14 -0700

    added body parser and some examples of working with query strings

---
+++ api/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var ctrlHotels = require('../controllers/hotels.controllers.js')
+
+router
+  .route('/hotels')
+  .get(ctrlHotels.hotelsGetAll);
+
+router
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne);
+
+router.
+  .route('/hotls/new')
+  .post(ctrlHotels.hotelsAddOne);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
-
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./api/routes');
 
 app.set('port', 3000);
 
+app.use(function(req, res, next){
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({ extended : false }));
 
+app.use('/api', routes);
 
-app.get('/json', function(req, res) {
-  res.json({"jsonData" : true});
-})
-
-app.get('/file', function(req, res) {
-  res.sendFile(path.join(__dirname, 'app.js'));
-})
 
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
