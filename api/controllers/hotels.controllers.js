8c97edee21f28526c7cd445541049064d790c20f
Paul <pacalabre@gmail.com>
2016-06-30 19:06:12 -0700

    added some error handling

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,10 +1,41 @@
 var mongoose = require('mongoose');
 var Hotel= mongoose.model('Hotel');
 
+var runGeoQuery = function(req, res) {
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance : 2000,
+    num : 5
+  }
+
+  Hotel
+    .geoNear(point, geoOptions, function(err, results, stats){
+      console.log('Geo results', results);
+      console.log('Geo stats', stats);
+      res
+        .status(200)
+        .json(results);
+    })
+}
+
 module.exports.hotelsGetAll = function(req, res) {
 
    var offset = 0;
    var count = 5;
+   var maxCount = 10;
+
+   if (req.query && req.query.lat && req.query.lng) {
+    runGeoQuery(req, res);
+    return;
+   }
 
 
    if(req.query && req.query.offset) {
@@ -13,15 +44,39 @@ module.exports.hotelsGetAll = function(req, res) {
    if(req.query && req.query.count) {
     count = parseInt(req.query.count, 10);
    }
+   if( isNaN(offset) || isNaN(count)){
+    res
+      .status(400)
+      .json({
+        "message" : "Search term should be a number."
+      });
+      return;
+   }
+
+   if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message" : "Count limit of " + maxCount + " exceeded."
+      })
+    return;
+   }
 
    Hotel
    .find()
    .skip(offset)
    .limit(count)
    .exec(function(err, hotels){
-    console.log("Found Hotels", hotels.length);
-    res
-      .json(hotels);
+    if(err) {
+      console.log("error finding hotels");
+      res
+        .status(500)
+        .json(err);
+    } else {
+      console.log("Found Hotels", hotels.length);
+      res
+        .json(hotels);
+    }
    });
 
    // collection
@@ -45,9 +100,23 @@ module.exports.hotelsGetOne = function(req, res) {
    Hotel
     .findById(hotelId)
     .exec(function(err, doc){
-      res
-      .status(200)
-      .json(doc);
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if(err) {
+      console.log("error finding hotel");
+      response.status = 500;
+      response.message = err;
+    } else if(!doc){
+        response.status = 404;
+        response.message = ({
+          "message" : "Hotel ID not found."
+        });
+    }
+    res
+      .status(response.status)
+      .json(response.message);
     });
 
 
