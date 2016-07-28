2e62f6415631c498b105e267748511cf07ca31c5
Paul <pacalabre@gmail.com>
2016-07-27 19:46:24 -0700

    fixed issue with info from the HotelsController not working - controller was missing the 'r'

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
-angular.module('myApp', ['ngRoute']).config(config);
+// angular.module('myApp', ['ngRoute']).config(config);
 
-function config($routeProvider) {
-  $routeProvider.when('/' ,  {
-    templateUrl : 'main/main.html',
-    controller : 'MainController',
-    controllerAs: 'vm'
-  }).when('/film/:id', {
-    templateUrl: 'film/film.html',
-    controller : 'FilmController',
-    controllerAs: 'vm'
-  })
-}
+// function config($routeProvider) {
+//   $routeProvider.when('/' ,  {
+//     templateUrl : 'main/main.html',
+//     controller : 'MainController',
+//     controllerAs: 'vm'
+//   }).when('/film/:id', {
+//     templateUrl: 'film/film.html',
+//     controller : 'FilmController',
+//     controllerAs: 'vm'
+//   })
+// }
